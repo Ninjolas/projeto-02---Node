d3efc65182c2fa454beca86a6d7fb39f705f5073
Nicolas_GG <nicolas.groff@senairs.org.br>
2026-01-14 15:02:07 -0300

    adição com mongoDB e mais funcionalides

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require( 'path');
+const mongoose = require('mongoose')
 var bodyParser = require('body-parser');
 
 const app = express();
+
+const Posts = require('./posts.js');
+
+mongoose.connect('mongodb+srv://ninjolas:gPhVrslZY4f7Ades@teste.z16kqae.mongodb.net/teste_notícias').then(()=>{
+    console.log('Conectado ao banco de dados');
+}).catch((err)=>{
+    console.log(err.message);
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,11 +21,23 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname , '/pages'));
 
-app.get("/", (req,res) =>{
-    console.log(req.query);
+app.get("/", async(req,res) =>{
+  
 
     if(req.query.busca == null){
-        res.render('home', {});
+        let posts = await Posts.find({}).sort({'_id':-1}).exec()
+        posts = posts.map((val)=>{
+            return {
+                titulo:val.titulo,
+                imagem:val.imagem,
+                categoria:val.categoria,
+                conteudo:val.conteudo,
+                slug:val.slug,
+                descricaoCurta : val.conteudo.substring(0,100)
+            }
+        })
+        console.log(posts[0]);
+        res.render('home', {posts:posts});
     }
     else{
         res.render("busca", {});
