7b68101e4f4c99e8e2a61b874927ac96961e6939
Nicolas Grillo Groff <nicolasgg04@gmail.com>
2026-01-13 23:13:13 -0300

    começo front-end

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname,'public')));
-app.set('views', path.join(__dirname , '/views'));
+app.set('views', path.join(__dirname , '/pages'));
 
 app.get("/", (req,res) =>{
     console.log(req.query);
 
-    if(req.query.oloco == null){
-        res.send('home');
+    if(req.query.busca == null){
+        res.render('home', {});
     }
     else{
-        res.send("Você está buscando: "+req.query.oloco)
+        res.send("Você está buscando: "+req.query.busca)
     }
 })
 
